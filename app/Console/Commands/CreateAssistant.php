918f8f99a119d9591c4875f24113ba8cf84ce96b
daviddeveloper001 <deivydeveloper@gmail.com>
2025-01-04 19:05:22 -0500

    CreateAssistant

--- app/Console/Commands/CreateAssistant.php
+++ app/Console/Commands/CreateAssistant.php
@@ -3,28 +3,47 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use OpenAI\Laravel\Facades\OpenAI;
 
 class CreateAssistant extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'app:create-assistant';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
+    protected $signature = 'app:create-assistant {file_id}';
+
+
     protected $description = 'Command description';
 
-    /**
-     * Execute the console command.
-     */
     public function handle()
     {
-        //
+        /* $models = OpenAI::models()->list();
+
+        foreach ($models->data as $model) {
+            $this->info('Model ID: ' . $model->id);
+        } */
+        // Obtenemos el ID del archivo pasado como argumento
+        $fileId = $this->argument('file_id');
+
+        // Creamos el asistente
+        $assistant = OpenAI::assistants()->create([
+            'name' => 'LaraBOT',
+            'tools' => [
+                [
+                    'type' => 'code_interpreter', // Definimos la herramienta
+                ],
+            ],
+            'tool_resources' => [
+                'code_interpreter' => [ // Vinculamos los recursos al tipo de herramienta
+                    'file_ids' => [$fileId], // Pasamos el ID del archivo subido
+                ],
+            ],
+            'instructions' => 'Eres un bot útil que ayuda a desarrolladores que utilizan el framework Laravel. 
+                Puedes responder preguntas sobre el framework y ayudarles a encontrar la documentación adecuada. 
+                Usa los archivos subidos para responder las preguntas. Las respuestas siempre deben estar en español.',
+
+            'model' => 'gpt-4o-mini-2024-07-18',
+        ]);
+
+        // Mostramos el ID del asistente creado
+        $this->info('Assistant ID: ' . $assistant->id);
     }
 }
